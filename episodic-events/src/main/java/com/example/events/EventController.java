05ecccac6ec7c66750697109e70ba31802b6590b
Bittu Joju <bittujoju@gmail.com>
2017-05-23 10:35:24 -0600

    remove rabbit codes

--- episodic-events/src/main/java/com/example/events/EventController.java
+++ episodic-events/src/main/java/com/example/events/EventController.java
@@ -1,13 +1,9 @@
 package com.example.events;
 
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistrar;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.messaging.converter.MappingJackson2MessageConverter;
-import org.springframework.messaging.handler.annotation.support.DefaultMessageHandlerMethodFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
